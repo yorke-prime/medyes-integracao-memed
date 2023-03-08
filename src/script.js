db525893b5981f028a102f8149d4c6630122c45a
Franciel Santos Silva <francielvendas@outlook.com>
2023-03-08 11:39:23 -0300

    novo carregamento

--- src/script.js
+++ src/script.js
@@ -1,26 +1,28 @@
 console.log(MdSinapsePrescricao);
-if (MdSinapsePrescricao)
-  MdSinapsePrescricao.event.add('core:moduleInit', (module) => {
-    if (module.name === 'plataforma.prescricao') {
-      MdHub.command.send('plataforma.prescricao', 'setFeatureToggle', {
-        deletePatient: true,
-        removePatient: true,
-        editPatient: true,
-        buttonClose: true,
-      }); 
-                  
-      MdHub.command.send('plataforma.prescricao', 'setPaciente', {
-        external_id: paciente.external_id || '',
-        nome: paciente.nome || '',
-        cpf: paciente.cpf || '',
-        telefone: paciente.cpf || '',
-      }).then(function() {
-        MdHub.module.show('plataforma.prescricao');
-      });
-                  
-      MdHub.event.add('prescricaoImpressa', function(prescriptionData) {
-        console.log(prescriptionData)
-      });
-    }
-  });
-
+MdSinapsePrescricao.event.add('core:moduleInit', (module) => {
+  if (module.name === 'plataforma.prescricao') {
+    document.getElementById('status').innerHTML="Status: Iniciado"
+    MdHub.command.send('plataforma.prescricao', 'setFeatureToggle', {
+      deletePatient: true,
+      removePatient: true,
+      editPatient: true,
+      buttonClose: true,
+    });
+    
+                
+    MdHub.command.send('plataforma.prescricao', 'setPaciente', {
+      external_id: paciente.external_id || '',
+      nome: paciente.nome || '',
+      cpf: paciente.cpf || '',
+      telefone: paciente.cpf || '',
+    }).then(function() {
+      document.getElementById('status').innerHTML="Status: Paciente enviado"
+      MdHub.module.show('plataforma.prescricao');
+    });
+                
+    MdHub.event.add('prescricaoImpressa', function(prescriptionData) {
+      console.log(prescriptionData)
+    });
+  }
+});
+ 
